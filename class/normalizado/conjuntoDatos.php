46a3320223ae9f08b59af10542abea7a75ec7fb1
usuario <usuario@debianXen>
2018-01-26 16:14:08 +0100

    actualización

---
+++ class/normalizado/conjuntoDatos.php
@@ -0,0 +1,13 @@
+<?php 
+
+abstract class conjuntoDatos {
+    public $mono_segundos; 	// duración de cada tramo temporal, en segundos
+    public $archivo;		// ruta del archivo que contiene los datos
+    private $mono;		// actual límite de tramo a superar
+    public $tms_direccion; 	// sentido ascendente TRUE  descendente FALSE
+    private $maximos_datos; // max datos a procesar del archivo ( -1 para todos)
+    public $bufferDatos; // almacén de datos
+}
+
+?>
+
